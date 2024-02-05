90107c927881eec31357b8d95e6b4835d470cd4a
Amal221200 <amalmurikkoli2000@gmail.com>
2024-02-05 10:11:19 +0530

    deleting

--- my-rdk-query/src/components/OneProduct.jsx
+++
@@ -1,9 +0,0 @@
-import { useAddNewProductMutation, useDeleteProductMutation, useGetOneProductQuery, useUpdateProductMutation } from "../app/features/services/dummyData";
-
-const OneProduct = () => {
-    // const res = useGetOneProductQuery(1);
-    const [addNewProduct, res] = useAddNewProductMutation()
-    return (<h1 onClick={() => addNewProduct({title: "Hello There", views: 20})}>One Product</h1>);
-}
-
-export default OneProduct;
